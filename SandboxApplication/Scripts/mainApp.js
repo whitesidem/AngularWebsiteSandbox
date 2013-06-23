97fff1b09c65f0cebf4fded536b33e6d7909b834
Michael Whiteside <m.whiteside@blueyonder.co.uk>
2013-06-23 18:28:23 +0100

    filtering of datagrid,
    separate controller and list controllers that are not associated.
    Instead the filter values are shared via a singleton service.

--- SandboxApplication/Scripts/mainApp.js
+++ SandboxApplication/Scripts/mainApp.js
@@ -13,9 +13,67 @@
             .otherwise({ redirectTo: '/MainMenu' });
     } ]);
 
-    app.controller("MainController", function ($scope) {
-        $scope.myNmae = 'Mick';
+
+    app.factory("filterService", function () {
+        console.log('filter service create');
+        return {
+            activeFilters: {},
+            searchText: ''
+        };
     });
+
+    app.controller("MainController", function ($scope) {
+        $scope.controllerIs = 'MainController';
+    });
+
+    app.controller("FilterController", function ($scope, filterService) {
+        $scope.controllerIs = 'FilterController';
+
+        $scope.filterService = filterService;
+
+        $scope.prod = {
+            name: 'Micky triger Team Whiteside'
+        };
+    });
+
+    app.controller("ListController", function ($scope, filterService) {
+
+        $scope.controllerIs = 'ListController';
+
+        $scope.filterService = filterService;
+
+
+        $scope.productList = [
+            {
+                sku: "ASD_WH",
+                desc: "Awesome Washing machine",
+                applianceType: "Washing Machine",
+                brand: "Hotpoint",
+                isTop5: true
+            },
+            {
+                sku: "LKPEPE_WH",
+                desc: "Great Kettle",
+                applianceType: "Kettle",
+                brand: "Hotpoint",
+                isTop5: false
+            },
+            {
+                sku: "KLWEW_WH",
+                desc: "Nonsense Kettle",
+                applianceType: "Kettle",
+                brand: "samsung",
+                isTop5: true
+            }
+
+        ];
+
+    });
+
+    app.controller("ListFilterController", function ($scope, filterService) {
+        $scope.filterService = filterService;
+    });
+
 
 } ());
 
